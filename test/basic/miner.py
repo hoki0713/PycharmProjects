059822ab135236d23bb2e7b50f213fdab939b223
hoki0713 <hojeong0713@gmail.com>
2020-06-29 18:28:21 +0900

    0629 18:28

--- test/basic/miner.py
+++ test/basic/miner.py
@@ -1,4 +1,12 @@
 from dataclasses import dataclass
+import re
+from nltk.tokenize import word_tokenize
+from konlpy.tag import Okt
+import pandas as pd
+from nltk import FreqDist
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 
 @dataclass
 class Model:
